9e79ae24a2d0680ae6e89c4fd17b5253f63d05e0
Ivan Shatilov <shatilov.ivan2oo1@gmail.com>
2025-11-13 16:33:08 +0600

    chore: add variable in .env to which we can pass the openai model name

--- src/mastra/agents/aiStorySplitterAgent.ts
+++ src/mastra/agents/aiStorySplitterAgent.ts
@@ -2,8 +2,10 @@ import { Agent } from "@mastra/core/agent";
 import { openai } from '@ai-sdk/openai';
 import { aiStorySplitterSystemPrompt } from "../prompts/agents/aiStorySplitterPrompts";
 
+const model = process.env.OPENAI_MODEL_NAME || "o4-mini"
+
 export const aiStorySplitterAgent = new Agent({
   name: "aiStorySplitterAgent",
-  model: openai("gpt-4o-mini"), 
+  model: openai(model), 
   instructions: aiStorySplitterSystemPrompt(),
 });
