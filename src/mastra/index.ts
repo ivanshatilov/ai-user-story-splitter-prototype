2f3347a8f914ff727b46c1eecfe601ed72e2d942
Ivan Shatilov <shatilov.ivan2oo1@gmail.com>
2025-11-13 14:46:16 +0600

    chore: add workflow to index.ts

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -3,9 +3,10 @@ import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { aiStorySplitterAgent } from './agents/aiStorySplitterAgent';
 import { taskCreationIntentScorer } from './scorers/taskCreationIntentScorer';
+import { aiStorySplitterWorkflow } from './workflows/aiStorySplitterWorkflow';
 
 export const mastra = new Mastra({
-  workflows: { },
+  workflows: { aiStorySplitterWorkflow },
   agents: { aiStorySplitterAgent },
   scorers: { taskCreationIntentScorer },
   logger: new PinoLogger({
