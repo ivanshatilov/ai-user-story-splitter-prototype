1be59d788d8e41adc1ef49c97018c37a391c9e57
Ivan Shatilov <shatilov.ivan2oo1@gmail.com>
2025-11-13 16:15:43 +0600

    feat: add branch into aiStorySplitterWorkflow that choses between two steps

--- src/mastra/workflows/aiStorySplitterWorkflow.ts
+++ src/mastra/workflows/aiStorySplitterWorkflow.ts
@@ -1,6 +1,10 @@
 import { createWorkflow } from "@mastra/core";
 import { aiStorySplitterWorkflowInputSchema, aiStorySplitterWorkflowOutputSchema } from "../schemas/aiStorySplitterWorkflow";
 import { taskCreationIntentStep } from "./steps/taskCreationIntentStep";
+import { createEpicTaskStep } from "./steps/createEpicTaskStep";
+import { smallTalkStep } from "./steps/smallTalkStep";
+
+const TASK_CREATION_INTENT_SCORE = 0.5;
 
 export const aiStorySplitterWorkflow = createWorkflow({
   id: "aiStorySplitterWorkflow",
@@ -8,5 +12,8 @@ export const aiStorySplitterWorkflow = createWorkflow({
   outputSchema: aiStorySplitterWorkflowOutputSchema,
 })
     .then(taskCreationIntentStep)
-
-aiStorySplitterWorkflow.commit();
+    .branch([
+        [ async ({ inputData }) => inputData.score >= TASK_CREATION_INTENT_SCORE, createEpicTaskStep],
+        [ async ({ inputData }) => inputData.score < TASK_CREATION_INTENT_SCORE, smallTalkStep],
+    ])
+    .commit();
